567ffb03243bd6b73f9ea36dc92bb20fa36d3c0d
Simone D <monatru3@hotmail.com>
2018-02-10 20:44:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,22 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+var price=Math.floor(Math.random()*100+1)
+cart.push({[item]: price})
+console.log(`${item} has been added to your cart.`)
+return cart
 }
 
 function viewCart() {
-  // write your code here
+
 }
 
 function total() {
   // write your code here
+  for (var i=0; i<cart.length; i++) {
+    var value += cart[i][0]
+    return value
+  }
 }
 
 function removeFromCart(item) {
